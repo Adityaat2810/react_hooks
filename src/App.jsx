7c485a6b1de311c0d84296bd5556441a961d9e52
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-05-15 14:58:25 +0530

    ugly solution for counter and sum of n numbers problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,25 @@
 import { useState } from "react";
-import { useEffect } from "react";
 
 function App() {
-  // state ?
-
-  const [todoId, setTodoId] = useState(1);
-  return <div>
-      <button onClick={() => setTodoId(1)}>1</button>
-      <button onClick={() => setTodoId(2)}>2</button>
-      <button onClick={() => setTodoId(3)}>3</button>
-      <button onClick={() => setTodoId(4)}>4</button>
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
 
 
-    <Todo id={todoId} />
-  </div>
-}
-
-function Todo({id}) {
-  const [todo, setTodo] = useState({});
-
-  useEffect(() => {
-    fetch("https://sum-server.100xdevs.com/todo?id=" + id)
-      .then(async function(res) {
-        const json = await res.json();
-        setTodo (json.todo);
-      })
-  }, [id])  // do not forget to add id as dependency in depedency array
+  let count = 0;
+  for (let i = 1; i <= inputValue; i++) {
+    count = count + i;
+  }
 
   return <div>
-    Id:{id}
-    <h1>
-      {todo.title}
-    </h1>
-    <h4>
-      {todo.description}
-    </h4>
+    <input onChange={function(e) {
+      setInputValue(e.target.value);
+    }} placeholder={"Find sum from 1 to n"}></input>
+    <br />
+    Sum from 1 to {inputValue} is {count}
+    <br />
+    <button onClick={() => {
+      setCounter(counter + 1);
+    }}>Counter ({counter})</button>
   </div>
 }
 
