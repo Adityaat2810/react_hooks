da0ae2afa62d47d651586359af0f9b326c069a7a
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-05-15 15:16:36 +0530

    solved problem in previous approach using useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import { useState ,useMemo } from "react";
+
+/**
+ *problem with previous approach when we click on counter
+ *rerender happen it again need to run an epensive 
+ * for loop to compute 
+ * 
+ * Solution =>  memoizataion 
+ * useMemo
+ * */
 
 function App() {
   const [counter, setCounter] = useState(0);
   const [inputValue, setInputValue] = useState(1);
 
 
-  let count = 0;
-  for (let i = 1; i <= inputValue; i++) {
-    count = count + i;
-  }
+  // this will run only when inputValue changes 
+  //very close to useEffect
+  let count =useMemo(()=>{
+    let finalCount = 0;
+    for (let i = 1; i <= inputValue; i++) {
+      finalCount = finalCount + i;
+    }
+
+    return finalCount;
+  },[inputValue])
+ 
 
   return <div>
     <input onChange={function(e) {
