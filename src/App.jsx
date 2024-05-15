6c9c5d8af275514cbe307f2b9afbddb18bd69d84
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-05-15 17:46:15 +0530

    useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,27 @@
-import { useState ,useMemo } from "react";
-
-/**
- *problem with previous approach when we click on counter
- *rerender happen it again need to run an epensive 
- * for loop to compute 
- * 
- * Solution =>  memoizataion 
- * useMemo
- * */
+import { memo, useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [inputValue, setInputValue] = useState(1);
+  const [count, setCount] = useState(0)
 
-
-  // this will run only when inputValue changes 
-  //very close to useEffect
-  let count =useMemo(()=>{
-    let finalCount = 0;
-    for (let i = 1; i <= inputValue; i++) {
-      finalCount = finalCount + i;
-    }
-
-    return finalCount;
-  },[inputValue])
- 
+  function onClick() {
+    console.log("child clicked")
+  }
 
   return <div>
-    <input onChange={function(e) {
-      setInputValue(e.target.value);
-    }} placeholder={"Find sum from 1 to n"}></input>
-    <br />
-    Sum from 1 to {inputValue} is {count}
-    <br />
+    <Child onClick={onClick} />
     <button onClick={() => {
-      setCounter(counter + 1);
-    }}>Counter ({counter})</button>
+      setCount(count + 1);
+    }}>Click me {count}</button>
   </div>
 }
+
+const Child = memo(
+  ({onClick}) => {
+    console.log("child render")
+
+  return <div>
+    <button onClick={onClick}>Button clicked</button>
+  </div>
+})
 
 export default App;
